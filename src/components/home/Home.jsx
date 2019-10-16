da72409e8297b2cfa5825490b743f898dc684c07
Bruno Gabriel <bruno.glm_@hotmail.com>
2019-10-16 01:38:13 -0300

    Criando o app crud em React

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Main from '../template/Main'
+
+const headerProps = {
+  icon: 'home',
+  title: 'Início',
+  subtitle: 'Segundo Projeto do capítulo de React.'
+}
+
+export default props => {
+  return (
+    <Main {...headerProps}>
+      <div className="display-4">Bem Vindo!</div>
+      <hr />
+      <p className="mb-0">
+        Sistema para exemplificar a construção de um cadastro desenvolvido em
+        React
+      </p>
+    </Main>
+  )
+}
